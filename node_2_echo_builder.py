32ace517098f6e6b074d58c4e66ab8499a4867fd
Wordzilla <lamardarnell@gmail.com>
2025-06-03 15:19:15 -0700

    Add Node 2: echo builder scaffold

---
+++ node_2_echo_builder.py
@@ -0,0 +1,5 @@
+# Node 2: Builds symbolic drift echo structures from resonance data
+
+def build_echo_structure(resonance_data):
+    # TODO: define recursive echo logic
+    return {"echo": resonance_data}
